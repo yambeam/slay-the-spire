47c0996d12e258a14e6862343ce9821056acd39f
Dale-YJ <1979603191@qq.com>
2026-05-20 08:34:20 +0800

    volume slider

--- scripts/settings/volumeSlider.gd
+++ scripts/settings/volumeSlider.gd
@@ -1,7 +1,13 @@
+class_name VolumeSlider
 extends HSlider
 
 @export var bus:StringName="Master"
 @onready var bus_index:=AudioServer.get_bus_index(bus)
+signal volume_changed
+
+
+func VolumeValue()->float:
+	return MusicPlayer.get_volume(bus_index)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,5 +16,6 @@ func _ready() -> void:
 	value_changed.connect(func(v:float):
 		MusicPlayer.set_volume(bus_index,v)
 		GameManager.save_config()
+		volume_changed.emit()
 	)
 	

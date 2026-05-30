16a5d0ac7cd42153923886a76c842cd3903cf8ea
yambeam <2456927015@qq.com>
2026-05-30 16:31:08 +0800

    .

--- entities/relics/colorless/活动星图.gd
+++ entities/relics/colorless/活动星图.gd
@@ -2,6 +2,10 @@ extends Relic
 
 func initialize_relic(owner: RelicUI) -> void:
 	Events.unknown_room_entered.connect(_on_unknown_room_enterd.bind(owner))
+	
+func deactivate_relic(_owner: RelicUI) -> void:
+	if Events.unknown_room_entered.is_connected(_on_unknown_room_enterd):
+		Events.unknown_room_entered.disconnect(_on_unknown_room_enterd)
 
 func _on_unknown_room_enterd(_room, _run_stats, char_stats: CharacterStats, owner: RelicUI) -> void:
 	char_stats.health += 5
